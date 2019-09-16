1dcabbc85db766182a43481ad4d9c52f8b09d033
Bombbery <jf45008@gmail.com>
2019-09-16 01:27:02 -0400

    Added files.

---
+++ README.md
@@ -0,0 +1,16 @@
+# OneShot Utility 2.0
+Recreation of the original OS Utility tool for command-line use.
+
+## Installation & Removal
+### User Release
+1. Download the OS_Utility_v2.0.exe 
+2. Save it to a location of your choosing.
+3. Run the .exe, and voila!
+
+### Development Version / Bleeding Edge
+1. Clone the Git repository via ```git clone https://github.com/Bombbery/OneShot-Utility-2.0```
+2. Install [Python 3.6.7](https://www.python.org/downloads/release/python-367/)
+3. Open a Command Prompt (Windows) in the cloned directory, and type the following:
+```python main.py```
+
+```git pull``` can be run to retrieve latest versions without the need for new releases.
---
+++ main.py
@@ -0,0 +1,35 @@
+"""
+OneShot Utility v2.0
+Author: Bombbery#0845
+
+Recreation of the original "OS Utility" resource in the form of a command-line tool.
+"""
+
+import os
+#path = os.path.expanduser('~/Documents')
+#os.chdir(path)
+#print(os.getcwd())
+
+def savePurge():
+    """
+    Purges all OneShot save files.
+    """
+    pass
+
+def reloadGame():
+    """
+    1. Terminates running OneShot process(es)
+    2. Runs savePurge()
+    3. Runs OneShot's .EXE
+    """
+    pass
+
+while True:
+    # Print x374513895713985
+    print('--------------')
+    print('OneShot Utility v2.0')
+    print('--------------')
+    print('''Option Select:
+    1. Delete Saves
+    2. Quick Reload
+    3. C''')
